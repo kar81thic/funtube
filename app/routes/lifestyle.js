3a30beb6926afb8e7923e78a31db5df84f52a2a9
Annadurai <annaduraimuthusamy@iMac.local>
2016-10-02 16:07:29 +0530

    today change

--- app/routes/lifestyle.js
+++ app/routes/lifestyle.js
@@ -1,18 +1,55 @@
 import Ember from 'ember';
-
+const PAGE_SIZE = 20;
 export default Ember.Route.extend({
-  model(){
-    return this.store.query('video', {filter: {Catogory: 'Amazing'}});
-  },
 
+  startAt: null,
+  endAt: null,
   actions: {
-    prev: function () {
-      Ember.Logger.log('Previous Button');
-      //TODO: Previous button action logic goes here...
-     },
-     next: function () {
-       Ember.Logger.log('Next Button');
-       //TODO: Next button action logic goes here...
-     }
+
+  saveInvitation() {
+      const newInvitation = this.store.createRecord('video', {videoId:'1', Catogory: 'test',  Description: 'test',
+        link: 'test', thmbnailLink:'test'});
+                newInvitation.save();
+    },
+
+  prev: function () {
+     var id = this.get('currentModel').get('firstObject.id');
+     this.set('startAt', null);
+     this.set('endAt', id);
+     this.refresh();
+   },
+
+   next: function () {
+     var id = this.get('currentModel').get('lastObject.id');
+     this.set('startAt', id);
+     this.set('endAt', null);
+     this.refresh();
    }
+
+    },
+    model() {
+
+      var query = {
+        limitToFirst: PAGE_SIZE+1
+      };
+
+      if (this.get('startAt')) {
+        query.startAt = this.get('startAt');
+      }
+
+      if (this.get('endAt')) {
+        query.endAt = this.get('endAt');
+        delete query.limitToFirst;
+        query.limitToLast = PAGE_SIZE+1;
+      }
+
+      return this.store.query('video', query).then((videos) => {
+        if (this.get('startAt')) {
+          return videos.slice(1);
+        } else {
+          return videos.slice(0, videos.get('length')-1);
+        }
+      });
+      //return this.store.findAll('video');
+    }
 });

a77a5001f82f05f137ed51c00c2357909826df93
Pradeep Subramaniam <kspradeepbe@gmail.com>
2016-10-02 17:38:16 +0530

    Update

--- app/components/videos-table.js
+++ app/components/videos-table.js
@@ -7,22 +7,20 @@ const {
 export default Ember.Component.extend({
 
   classNames: ['video--container'],
-  
+
   category: null,
 
   filterVideos: computed('videos.[]', {
     get(){
       const videos = this.get('videos');
-      const category = this.get('category') || 'All';
-      let filterVideos = (category !== 'All') ? videos.filterBy('Catogory', category) : videos;
-      var resultArr =  Ember.A();
-      var temp = []
-      filterVideos.forEach( (video, index)=>{
+      var temp = [];
+      var resultArr = [];
+      videos.forEach( (video, index)=>{
         temp.push(video);
         if(index !== 0 && (index + 1) % 4 === 0){
           resultArr.push(temp);
           temp = [];
-        }else if(index === (filterVideos.get('length') - 1)){
+        }else if(index === (videos.get('length') - 1)){
            resultArr.push(temp);
            temp = [];
          }
--- app/routes/lifestyle.js
+++ app/routes/lifestyle.js
@@ -1,40 +1,40 @@
 import Ember from 'ember';
-const PAGE_SIZE = 20;
+const PAGE_SIZE = 100;
 export default Ember.Route.extend({
 
   startAt: null,
   endAt: null,
   actions: {
 
-  saveInvitation() {
-      const newInvitation = this.store.createRecord('video', {videoId:'1', Catogory: 'test',  Description: 'test',
-        link: 'test', thmbnailLink:'test'});
-                newInvitation.save();
-    },
+    saveInvitation() {
+        const newInvitation = this.store.createRecord('video', {videoId:'1', Catogory: 'test',  Description: 'test',
+          link: 'test', thmbnailLink:'test'});
+                  newInvitation.save();
+      },
 
-  prev: function () {
-     const model = this.get('currentModel');
-     const lifeStyleVideos = model.filterBy('Catogory', 'Lifestyle');
-     let id;
-     if(lifeStyleVideos.get('length') > 0){
-        id = lifeStyleVideos.get('firstObject.id');
-     }
-     this.set('startAt', null);
-     this.set('endAt', id);
-     this.refresh();
-   },
+    prev: function () {
+       const model = this.get('currentModel');
+       const lifeStyleVideos = model.filterBy('Catogory', 'Lifestyle');
+       let id;
+       if(lifeStyleVideos.get('length') > 0){
+          id = lifeStyleVideos.get('firstObject.id');
+       }
+       this.set('startAt', null);
+       this.set('endAt', id);
+       this.refresh();
+     },
 
-   next: function () {
-     const model = this.get('currentModel');
-     const lifeStyleVideos = model.filterBy('Catogory', 'Lifestyle');
-     let id;
-     if(lifeStyleVideos.get('length') > 0){
-        id = lifeStyleVideos.get('firstObject.id');
+     next: function () {
+       const model = this.get('currentModel');
+       const lifeStyleVideos = model.filterBy('Catogory', 'Lifestyle');
+       let id;
+       if(lifeStyleVideos.get('length') > 0){
+          id = lifeStyleVideos.get('firstObject.id');
+       }
+       this.set('startAt', id);
+       this.set('endAt', null);
+       this.refresh();
      }
-     this.set('startAt', id);
-     this.set('endAt', null);
-     this.refresh();
-   }
 
     },
     model() {
@@ -53,17 +53,15 @@ export default Ember.Route.extend({
         query.limitToLast = PAGE_SIZE+1;
       }
 
-      if(!this.get('startAt') && !this.get('endAt')){
-        //TODO: Change limit count as prefered
-        query.limitToFirst = 300;
-      }
-
       return this.store.query('video', query).then((videos) => {
+        let __videos = [];
         if (this.get('startAt')) {
-          return videos.slice(1);
+          __videos = videos.slice(1);
         } else {
-          return videos.slice(0, videos.get('length')-1);
+          __videos = videos.slice(0, videos.get('length')-1);
         }
+        let ret = __videos.filterBy('Catogory', 'Lifestyle');
+        return (ret.get('length') > 20) ? ret.slice(0, 20) : ret;
       });
     },
 });
